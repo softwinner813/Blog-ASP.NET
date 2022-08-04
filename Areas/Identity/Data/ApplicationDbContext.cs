198a179dc54db4ddf8844ce48c5a61b2152f4507
softwinner813 <winner813@protonmail.com>
2022-08-04 02:55:23 -0700

    Renamed Tables & Add Migration

--- Areas/Identity/Data/ApplicationDbContext.cs
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -17,5 +17,37 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        // Custom Table Names for Identify
+        builder.HasDefaultSchema("Indetify");
+
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "Users");
+        });
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Roles");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
     }
 }
